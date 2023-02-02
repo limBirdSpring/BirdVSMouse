2d9b654ce0b1694d43aa1961a2c7a32813bbfece
dmsxo0691 <cheonu0610@gmail.com>
2023-02-02 16:47:05 +0900

    feat : Item Data 추가

--- Assets/Scripts/Manager/Inventory.cs
+++ Assets/Scripts/Manager/Inventory.cs
@@ -19,7 +19,7 @@ public class Inventory : SingleTon<Inventory>
 
     private void UpdateItemGFX()
     {
-        itemImage.sprite = curSetItem.sprite;
+        itemImage.sprite = curSetItem.itemIcon;
     }
 
     //해당 아이템이 장착되어있는지 확인하기
