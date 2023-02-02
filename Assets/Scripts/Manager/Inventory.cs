1027d3fbd80fc5c18e579856a49c25350d6af0a3
BirdSpring <115616372+limBirdSpring@users.noreply.github.com>
2023-02-02 18:11:00 +0900

    feat : 인벤토리 장착확인함수 추가

--- Assets/Scripts/Manager/Inventory.cs
+++ Assets/Scripts/Manager/Inventory.cs
@@ -5,6 +5,9 @@ using UnityEngine.UIElements;
 
 public class Inventory : SingleTon<Inventory>
 {
+    [SerializeField]
+    private List<ItemData> itemDatas = new List<ItemData>();
+
     private ItemData curSetItem;
 
     [SerializeField]
@@ -25,6 +28,11 @@ public class Inventory : SingleTon<Inventory>
     //해당 아이템이 장착되어있는지 확인하기
     public bool isItemSet(string name)
     {
+        foreach (ItemData item in itemDatas)
+        {
+            if (item.itemName == name)
+                return true;
+        }
         return false;
     }
 }
