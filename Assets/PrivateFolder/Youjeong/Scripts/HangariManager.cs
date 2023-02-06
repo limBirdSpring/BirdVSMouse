decdc93049aa17f374e791ec4075e83a287800f5
youjeongCheon <115628032+youjeongCheon@users.noreply.github.com>
2023-02-06 15:41:48 +0900

    test : MiniGame 항아리 게임 테스트

---
+++ Assets/PrivateFolder/Youjeong/Scripts/HangariManager.cs
@@ -0,0 +1,10 @@
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HangariManager : MonoBehaviourPunCallbacks
+{
+    private ItemData ItemData;
+    
+}
