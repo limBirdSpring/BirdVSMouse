a07a0135831ee4088b79062a3a8957a2e6191f61
BirdSpring <115616372+limBirdSpring@users.noreply.github.com>
2023-02-09 21:33:34 +0900

    feat : 시간매니저 잘 작동하는거 확인

--- Assets/PrivateFolder/Saebom/Scripts/PlayGameManager.cs
+++ Assets/PrivateFolder/Saebom/Scripts/PlayGameManager.cs
@@ -287,15 +287,6 @@ namespace Saebom
 
 
 
-        //플레이어가 킬로 죽었을 경우 이 함수를 호출해줌
-        public void PlayerDie()
-        {
-            photonView.RPC("PlayerDieAndMasterPlayerListUpdate", RpcTarget.MasterClient, PhotonNetwork.LocalPlayer.GetPlayerNumber());
-
-            if (PhotonNetwork.IsMasterClient)
-                ScoreManager.Instance.MasterCurPlayerStateUpdate();
-        }
-
         //플레이어가 투표로 죽었을 때 호출할 함수
         public void PlayerDie(int index)
         {
