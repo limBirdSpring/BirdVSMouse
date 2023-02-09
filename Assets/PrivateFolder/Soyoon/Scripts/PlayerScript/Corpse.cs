e26dacd55df355081d70f50cea50153594b0220b
SoYoon Ahn <bluingpetal28@gmail.com>
2023-02-09 16:59:15 +0900

    feat : add corpse destroy function

--- Assets/PrivateFolder/Soyoon/Scripts/PlayerScript/Corpse.cs
+++ Assets/PrivateFolder/Soyoon/Scripts/PlayerScript/Corpse.cs
@@ -1,19 +1,21 @@
 using Photon.Pun;
 using Saebom;
 using SoYoon;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Corpse : MonoBehaviour
 {
+    public void DestroyCorpse()
+    {
+        PhotonNetwork.Destroy(gameObject);
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        //PlayGameManager playGameManager = GameObject.Find("PlayGameManager").GetComponent<PlayGameManager>();
-        if(collision.gameObject.name == "KillRangeCollider") // active state
+        if (collision.gameObject.layer == LayerMask.NameToLayer("KillRange")) // active state
         {
             // 스파이일 경우 : 감지되지 않음, 스파이가 아닐 경우 감지됨
-            if(collision.GetComponentInParent<PlayerControllerTest>() != null)
+            if (collision.GetComponentInParent<PlayerControllerTest>() != null)
             {
                 PlayerControllerTest controller = collision.GetComponentInParent<PlayerControllerTest>();
                 PhotonView photonView = collision.GetComponentInParent<PhotonView>();
@@ -22,12 +24,12 @@ public class Corpse : MonoBehaviour
             }
         }
 
-        if(collision.GetComponent<PlayerControllerTest>() != null)
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
         {
-            PlayerControllerTest controller = collision.GetComponentInParent<PlayerControllerTest>();
-            PhotonView photonView = collision.GetComponentInParent<PhotonView>();
-            if (controller.state == PlayerState.Inactive)
-                controller.FoundCorpse();
+            PlayerControllerTest controller = collision.GetComponent<PlayerControllerTest>();
+            PhotonView photonView = collision.GetComponent<PhotonView>();
+            if (controller.state != PlayerState.Inactive)
+                Saebom.MissionButton.Instance.MissionButtonOff();
         }
     }
 }
