77c3cf6288be73c8b15ad4204646db82fc948536
BirdSpring <115616372+limBirdSpring@users.noreply.github.com>
2023-02-11 20:09:22 +0900

    fix : 상점 출력 수정

--- Assets/Scripts/Manager/StoreManager.cs
+++ Assets/Scripts/Manager/StoreManager.cs
@@ -114,31 +114,31 @@ public class StoreManager : MonoBehaviour
  
         photo1Img.sprite = photoItem1.itemIcon;
         photo1Price.text = "\\" + photoItem1.price.ToString();
-        photo1Name.text = photoItem1.name;
+        photo1Name.text = photoItem1.itemName;
 
  
         photo2Img.sprite = photoItem2.itemIcon;
         photo2Price.text = "\\" + photoItem2.price.ToString();
-        photo2Name.text = photoItem2.name;
+        photo2Name.text = photoItem2.itemName;
 
 
         photo3Img.sprite = photoItem3.itemIcon;
         photo3Price.text = "\\" + photoItem3.price.ToString();
-        photo3Name.text = photoItem3.name;
+        photo3Name.text = photoItem3.itemName;
 
         badge1Img.sprite = badgeItem1.itemIcon;
         badge1Price.text = "\\" + badgeItem1.price.ToString();
-        badge1Name.text = badgeItem1.name;
+        badge1Name.text = badgeItem1.itemName;
 
 
         badge2Img.sprite = badgeItem2.itemIcon;
         badge2Price.text = "\\" + badgeItem2.price.ToString();
-        badge2Name.text = badgeItem2.name;
+        badge2Name.text = badgeItem2.itemName;
 
   
         badge3Img.sprite = badgeItem3.itemIcon;
         badge3Price.text = "\\" + badgeItem3.price.ToString();
-        badge3Name.text = badgeItem3.name;
+        badge3Name.text = badgeItem3.itemName;
 
 
 
