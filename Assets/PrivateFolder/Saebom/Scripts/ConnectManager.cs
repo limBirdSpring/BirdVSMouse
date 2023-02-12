ddc6e9f329e9e09424c4739e45f6158d8eb72d98
BirdSpring <115616372+limBirdSpring@users.noreply.github.com>
2023-02-12 13:31:30 +0900

    feat : 초상화 스크립터블오브젝트 수정

--- Assets/PrivateFolder/Saebom/Scripts/ConnectManager.cs
+++ Assets/PrivateFolder/Saebom/Scripts/ConnectManager.cs
@@ -1,5 +1,8 @@
 using Photon.Pun;
-
+using Photon.Pun.UtilityScripts;
+using Photon.Realtime;
+using Saebom;
+using SoYoon;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,8 +10,14 @@ using UnityEngine;
 public class ConnectManager : MonoBehaviourPunCallbacks
 {
     public override void OnDisconnected(Photon.Realtime.DisconnectCause cause)
-    { 
+    {
+        PlayGameManager.Instance.PlayerDie(PhotonNetwork.LocalPlayer.GetPlayerNumber());
         PhotonNetwork.LeaveRoom();
         Debug.Log(PhotonNetwork.PlayerList.Length);
     }
+
+  //  public override void OnPlayerLeftRoom(Player otherPlayer)
+  //  {
+  //      Debug.Log(PhotonNetwork.PlayerList.Length);
+  //  }
 }
