25ab43ff8e0bb4243de24dea27eeda35cb163cca
BirdSpring <115616372+limBirdSpring@users.noreply.github.com>
2023-02-13 01:39:19 +0900

    feat : 방씬에 세팅창 추가

--- Assets/PrivateFolder/Saebom/Scripts/SettingManager.cs
+++ Assets/PrivateFolder/Saebom/Scripts/SettingManager.cs
@@ -4,21 +4,26 @@ using UnityEngine;
 
 public class SettingManager : SingleTon<SettingManager>
 {
-    //이멀젼시 횟수
+    //이멀젼시 횟수 : 3,4,5
     public int emergencyCount = 3;
 
-    //시간이 빠르게 흐르는 데에 걸리는 방해성공 횟수
+    //시간이 빠르게 흐르는 데에 걸리는 방해성공 횟수 : 3,5,7
     public int successHiderance = 5;
     private int curHiderance = 5;
 
-    //한판 당 한정할 킬 수
+    //한판 당 한정할 킬 수 : 1,2
     public int killCount = 1;
 
-    //한 턴당 총 시간
+    //한 턴당 총 시간 : 360, 500, 700, 900
     public float turnTime = 360f;
 
-    //라운드 횟수 제한
+    //라운드 횟수 제한 : 8, 10, 12
     public int maxRoundCount = 8;
+
+    private void OnEnable()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
 
     public void OnEmergencyCountButtonClick(int count)
     {
