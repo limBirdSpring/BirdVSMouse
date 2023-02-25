78747a748d1e55d97b8f76defa87242a10a3beb7
SoYoon Ahn <bluingpetal28@gmail.com>
2023-02-25 23:33:35 +0900

    fix : fixed inactive player interaction with corpse

--- Assets/PrivateFolder/Soyoon/Scripts/PlayerScript/Corpse.cs
+++ Assets/PrivateFolder/Soyoon/Scripts/PlayerScript/Corpse.cs
@@ -1,4 +1,5 @@
 using Photon.Pun;
+using Saebom;
 using UnityEngine;
 
 namespace SoYoon
@@ -15,40 +16,31 @@ namespace SoYoon
             // 활동시기인 동물들은 모두 발견 가능 -> 신고(투표창)
             // 비활동시기인 동물들은 시체를 없애기 가능
             //Debug.Log(collision.gameObject.name);
-            if (collision.gameObject.layer == LayerMask.NameToLayer("Player") || collision.gameObject.layer == LayerMask.NameToLayer("InActive"))
-            {
-                //Debug.Log("On Collision");
-                PlayerControllerTest controller = collision.GetComponent<PlayerControllerTest>();
-                targetPhotonView = collision.GetComponent<PhotonView>();
+            if (!(collision.gameObject.name == PlayGameManager.Instance.myPlayerState.playerPrefab.name))
+                return;
 
-                if (targetPhotonView.IsMine)
+            //Debug.Log("On Collision");
+            PlayerControllerTest controller = collision.GetComponent<PlayerControllerTest>();
+            targetPhotonView = collision.gameObject.GetPhotonView();
+
+            if (targetPhotonView.IsMine)
+            {
+                if (controller.state == PlayerState.Active)
                 {
-                    if (controller.state == PlayerState.Active)
-                    {
-                        //Debug.Log("On active Collision");
-                        InterActionAdapter adapter = gameObject.GetComponent<InterActionAdapter>();
-                        adapter.OnInterAction.RemoveAllListeners();
-                        adapter.OnInterAction.AddListener(FoundCorpse);
-                    }
-                    else if(controller.state == PlayerState.Inactive)
-                    {
-                        //Debug.Log("On inactive Collision");
-                        InterActionAdapter adapter = gameObject.GetComponent<InterActionAdapter>();
-                        adapter.OnInterAction.RemoveAllListeners();
-                        adapter.OnInterAction.AddListener(DestroyCorpse);
-                    }
+                    //Debug.Log("On active Collision");
+                    InterActionAdapter adapter = gameObject.GetComponent<InterActionAdapter>();
+                    adapter.OnInterAction.RemoveAllListeners();
+                    adapter.OnInterAction.AddListener(FoundCorpse);
+                }
+                else if(controller.state == PlayerState.Inactive)
+                {
+                    //Debug.Log("On inactive Collision");
+                    InterActionAdapter adapter = gameObject.GetComponent<InterActionAdapter>();
+                    adapter.OnInterAction.RemoveAllListeners();
+                    adapter.OnInterAction.AddListener(DestroyCorpse);
                 }
             }
         }
-
-        //private void OnTriggerExit2D(Collider2D collision)
-        //{
-        //    if (collision.gameObject.layer == LayerMask.NameToLayer("Player") || collision.gameObject.layer == LayerMask.NameToLayer("InActive"))
-        //    {
-        //        if (targetPhotonView.IsMine)
-        //            Saebom.MissionButton.Instance.MissionButtonOff();
-        //    }
-        //}
 
         private void FoundCorpse()
         {
@@ -59,7 +51,7 @@ namespace SoYoon
 
         private void DestroyCorpse()
         {
-            targetPhotonView.RPC("DestroyCorpse", RpcTarget.All, playerNum);
+            targetPhotonView.RPC("DestroyCorpse", RpcTarget.All, playerNum, false);
             //Saebom.MissionButton.Instance.MissionButtonOff();
         }
     }
